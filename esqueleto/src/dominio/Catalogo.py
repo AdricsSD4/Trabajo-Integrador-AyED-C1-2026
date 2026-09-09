db027695b36ccbe2f70bfc064c599f81ee75a9b8
FrancoLautaro2112 <thefranco177@gmail.com>
2026-09-09 20:44:12 -0300

    Estructura 00

---
+++ esqueleto/src/dominio/Catalogo.py
@@ -0,0 +1,10 @@
+from src.dominio.Musica import Cancion
+
+class Catalogo:
+    def __init__(self):
+        self.canciones = [
+            Cancion(1, "De Música Ligera", "Soda Stereo", "Canción Animal"),
+            Cancion(2, "Persiana Americana", "Soda Stereo", "Signos"),]
+
+def playlist(self):
+    pass
---
+++ esqueleto/src/dominio/Musica.py
@@ -0,0 +1,8 @@
+class musica:
+    def __init__(self,id,titulo,artista,album):
+        self.id=id
+        self.titullo=titulo
+        self.artista=artista
+        self.album=album
+
+
